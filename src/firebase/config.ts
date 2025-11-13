549d7881eca992e9fe5e8b0766ac9a36a2fe351d
Razeen Zakariya <razeenzak@gmail.com>
2025-11-13 23:03:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7745130254-ab864",
+  "appId": "1:782164443778:web:4a51e064be6e3f2a5b2a9b",
+  "apiKey": "AIzaSyCcYPvAouSGKYYFz_IGgFH5gwfQnFMSJ8c",
+  "authDomain": "studio-7745130254-ab864.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "782164443778"
+};
